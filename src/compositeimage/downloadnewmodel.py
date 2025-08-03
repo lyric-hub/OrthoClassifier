3f63647b7751d9b7b4ab86cd72a87342b226272c
lyric-hub <csaju53@gmail.com>
2025-08-03 13:32:25 +0530

    Added file downloadnewmodel.py to easy download and save it to model/

---
+++ src/compositeimage/downloadnewmodel.py
@@ -0,0 +1,18 @@
+from compositeimage import load_config
+from compositeimage import getmodel
+import torch
+import os
+
+
+cfg = load_config()
+
+model = getmodel(
+    num_classes = cfg.model.num_classes,
+    model_name  = cfg.model.name,
+    pretrained  = cfg.model.pretrained
+)
+
+os.makedirs("models", exist_ok=True)
+save_path = os.path.join("models", f"{cfg.model.name}.pt")
+torch.save(model.state_dict(), save_path)
+print(f"Model weights saved to {save_path}")
